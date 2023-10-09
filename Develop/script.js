87a3511b81644c19914bbd4b5aceda0c145caef1
rnahill <rei333lly@gmail.com>
2023-10-09 18:10:21 -0400

    started writing function and if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// create function
+// give prompts for different password criteria:
+//  prompt for length of password between 8-128 characters
+//  confirm whether or not to use uppercase, lowercase, numbers, and/or, special characters
+// random password generated
+// display password
+
+
+function createPassword() {
+
+  var chooseLength = window.prompt("What length would you like?");
+  var chooseUpperCase = window.confirm("Click OK to include uppercase letters. Click cancel to not include uppercase letters.");
+  var chooseLowerCase = window.confirm("Click OK to include lowercase letters. Click cancel to not include lowercase letters.");
+  var chooseNumbers = window.confirm("Click OK to include numbers. Click cancel to not include numbers.");
+  var chooseSpecialChar = window.confirm("Click OK to include special characters. Click cancel to not include special characters.");
+  var specialChar = [ "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "`", "{", "|", "}", "~" ]
+  // [ ] " / all left out
+
+  // length
+  if (chooseLength > 0 && chooseLength < 128) {
+      console.log(chooseLength);
+  
+  }
+  
+  else {
+      window.alert("Number not valid! Please choose a number between 0 and 128.");
+    
+  }
+
+  // uppercase
+  if(chooseUpperCase) {
+    
+  }
+
+//  lowercase
+  if(chooseLowerCase) {
+
+  }
+
+  // numbers
+  if(chooseNumbers) {
+
+  }
+
+//  special characters
+  if(chooseSpecialChar) {
+    // for loop here
+  
+  }
+
+
+}
+
+createPassword();
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
